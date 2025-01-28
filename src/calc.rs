726c397fdf6ecf966854257ed3096e3c0c8547a6
exdisj <exdisj@outlook.com>
2025-01-28 10:00:01 -0500

    Started testing.

--- src/calc.rs
+++ src/calc.rs
@@ -80,7 +80,7 @@ impl Mul for VariableUnion {
         match (self, rhs) {
             (Self::Vec(a), Self::Sca(b)) | (Self::Sca(b), Self::Vec(a)) => Ok(Self::Vec(a * b)),
             (Self::Mat(a), Self::Sca(b)) | (Self::Sca(b), Self::Mat(a)) => Ok(Self::Mat(a * b)),
-            (a, b) => a.get_ref().add(b.get_ref())
+            (a, b) => a.get_ref().mul(b.get_ref())
         }
     }
 }
@@ -323,7 +323,75 @@ pub enum VariableUnionRefMut<'a> {
 
 #[test]
 fn test_variable_union() {
+    //We are primarily testing the operators.
+    let ar = Scalar::new(4.0);
+    let br = Complex::new(1.0, 2.5);
+    let cr = MathVector::from(vec![1, 4, 3]);
+    let dr = Matrix::identity(2);
 
+    // Conversion
+    let a = VariableUnion::from(ar);
+    let b = VariableUnion::from(br.clone());
+    let c = VariableUnion::from(cr.clone());
+    let d = VariableUnion::from(dr.clone());
+
+    assert_eq!(a, VariableUnion::Sca(ar));
+    assert_eq!(b, VariableUnion::Cmp(br.clone()));
+    assert_eq!(c, VariableUnion::Vec(cr.clone()));
+    assert_eq!(d, VariableUnion::Mat(dr.clone()));
+
+    //Printing
+    assert_eq!(format!("{:?}", &a), format!("{:?}", &ar));
+    assert_eq!(format!("{:?}", &b), format!("{:?}", &br));
+    assert_eq!(format!("{:?}", &c), format!("{:?}", &cr));
+    assert_eq!(format!("{:?}", &d), format!("{:?}", &dr));
+
+    assert_eq!(format!("{}", &a), format!("{}", &ar));
+    assert_eq!(format!("{}", &b), format!("{}", &br));
+    assert_eq!(format!("{}", &c), format!("{}", &cr));
+    assert_eq!(format!("{}", &d), format!("{}", &dr));
+
+    // Negation
+    assert_eq!(-a.clone(), VariableUnion::Sca(-ar));
+    assert_eq!(-b.clone(), VariableUnion::Cmp(-br.clone()));
+    assert_eq!(-c.clone(), VariableUnion::Vec(-cr.clone()));
+    assert_eq!(-d.clone(), VariableUnion::Mat(-dr.clone()));
+
+
+    let er = Scalar::new(1.0);
+    let fr = Complex::new(2.1, 4.0);
+    let gr = MathVector::from(vec![2, 1, -4]);
+    let hr = Matrix::try_from(vec![vec![1, 4], vec![5,2]]).unwrap();
+
+    //Addition
+    assert_eq!(a.clone() + VariableUnion::from(er), Ok( VariableUnion::from( ar + er) ));
+    assert_eq!(b.clone() + VariableUnion::from(fr.clone()), Ok( VariableUnion::from( &br + &fr ) ));
+    assert_eq!(c.clone() + VariableUnion::from(gr.clone()), Ok( VariableUnion::from( &cr + &gr ) ));
+    assert_eq!(d.clone() + VariableUnion::from(hr.clone()), Ok( VariableUnion::from( (&dr + &hr).unwrap() ) ));
+
+    //Subtraction
+    assert_eq!(a.clone() - VariableUnion::from(er), Ok( VariableUnion::from( ar - er) ));
+    assert_eq!(b.clone() - VariableUnion::from(fr.clone()), Ok( VariableUnion::from( &br - &fr ) ));
+    assert_eq!(c.clone() - VariableUnion::from(gr.clone()), Ok( VariableUnion::from( &cr - &gr ) ));
+    assert_eq!(d.clone() - VariableUnion::from(hr.clone()), Ok( VariableUnion::from( (&dr - &hr).unwrap() ) ));
+
+    //Multiplication
+    assert_eq!(a.clone() * VariableUnion::from(er), Ok( VariableUnion::from( ar * er) ));
+    assert_eq!(b.clone() * VariableUnion::from(fr.clone()), Ok( VariableUnion::from( &br * &fr ) ));
+    assert_eq!(b.clone() * VariableUnion::Cmp(ar.into()), Ok( VariableUnion::from( &br * &ar.into() ) ));
+    assert_eq!(c.clone() * VariableUnion::from(ar), Ok( VariableUnion::from( &cr * ar ) ));
+    assert!( (c.clone() * VariableUnion::from(gr.clone())).is_err() );
+    assert_eq!(d.clone() * VariableUnion::from(hr.clone()), Ok( VariableUnion::from( (&dr * &hr).unwrap() ) ));
+    assert_eq!(d.clone() * VariableUnion::from(ar), Ok( VariableUnion::from( &dr * ar ) ));
+
+    //Division
+    assert_eq!(a.clone() / VariableUnion::from(er), Ok( VariableUnion::from( ar / er) ));
+    assert_eq!(b.clone() / VariableUnion::from(fr.clone()), Ok( VariableUnion::from( &br / &fr ) ));
+    assert_eq!(b.clone() / VariableUnion::Cmp(ar.into()), Ok( VariableUnion::from( &br / &ar.into() ) ));
+    assert!( (c.clone() / VariableUnion::from(gr.clone())).is_err() );
+    assert_eq!(c.clone() / VariableUnion::from(ar), Ok( VariableUnion::from( &cr / ar ) ));
+    assert!( (d.clone() / VariableUnion::from(hr.clone())).is_err() );
+    assert_eq!(d.clone() / VariableUnion::from(ar), Ok( VariableUnion::from( &dr / ar ) ));
 }
 
 #[test]
